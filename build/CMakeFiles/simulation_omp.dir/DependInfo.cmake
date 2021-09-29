9214b0b819600cb2f7ade264284c8a34849ae017
Angeloloi19 <angeloloi19@gmail.com>
2021-09-29 14:29:29 +0200

    modifications for running with hydra 3

---
+++ build/CMakeFiles/simulation_omp.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/atlantis/Scrivania/tcode_2021/TCode/src/simulation.cpp" "/home/atlantis/Scrivania/tcode_2021/TCode/build/CMakeFiles/simulation_omp.dir/src/simulation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/atlantis/tools/Hydra-3.2.1"
+  "/home/atlantis/tools/root/install/include"
+  "../"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
